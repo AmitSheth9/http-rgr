d7b7fe51c2db54fd34f84d9b84cdce9dc036784d
AmitSheth9 <amit.sheth9@gmail.com>
2021-11-05 10:55:20 -0700

    cleaned code

--- index.js
+++ index.js
@@ -1,20 +1,8 @@
 function getMethod(request) {
   const splitArr = request.split('\n');
-  console.log(splitArr);
   const splitArr2 = splitArr[1].split(' ');
-  console.log(splitArr2);
-  console.log(splitArr2[0]);
   return splitArr2[0];
 }
 
-const request = `
-DELETE / HTTP/1.1
-Host: example.com
-Accept-Language: us-en`;
-const request2 = `
-GET / HTTP/1.1
-Host: example.com
-Accept-Language: us-en`;
-console.log(getMethod(request2));
-console.log(getMethod(request));
+
 module.exports = getMethod;
--- index.test.js
+++ index.test.js
@@ -3,7 +3,7 @@ const getMethod = require('./index');
 
 
 describe('get the method of http request', () => {
-  it('takes an http request and returns a function that gets the method', async() => {
+  it('takes an http request and returns a function that gets the method', () => {
     const request1 = `
 DELETE / HTTP/1.1
 Host: example.com
@@ -12,4 +12,7 @@ Accept-Language: us-en`;
       
     expect(getMethod(request1)).toEqual('DELETE');
   });
+
+  
 });
+
